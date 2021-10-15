30ba2037d9f3772407c9406e3ebe128a58931bc6
kamal <kamal@tef>
2021-10-15 19:35:56 +0530

    CORS added

--- customer/src/main/java/com/kamalsblog/classicmodels/CustomerController.java
+++ customer/src/main/java/com/kamalsblog/classicmodels/CustomerController.java
@@ -13,6 +13,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/customer")
 public class CustomerController {
 
