c4493b00d47bbb648317be71580016e2ff5867c6
kamal <kamal@tef>
2021-10-17 22:59:39 +0530

    cloud run commit

--- customer/src/main/java/com/kamalsblog/classicmodels/CustomerController.java
+++ customer/src/main/java/com/kamalsblog/classicmodels/CustomerController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins="*")
 @RequestMapping("/customer")
 public class CustomerController {
 
