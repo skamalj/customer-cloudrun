5ae6b6020eca7a852d8df4f89c420a458ac1ebd2
kamal <kamal@tef>
2021-10-18 15:14:11 +0530

    cloud run commit

--- customer/src/main/java/com/kamalsblog/classicmodels/CustomerController.java
+++ customer/src/main/java/com/kamalsblog/classicmodels/CustomerController.java
@@ -3,24 +3,22 @@ package com.kamalsblog.classicmodels;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.validation.FieldError;
+
 import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,11 +32,6 @@ public class CustomerController {
 	@Autowired
 	public CustomerController(MongoTemplate template, CustomerRepository repository) {
 		this.repository = repository;
-	}
-	
-	@RequestMapping(value = "/**",method = RequestMethod.OPTIONS)     
-	public ResponseEntity handle() {         
-		return new ResponseEntity(HttpStatus.OK);     
 	}
 
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
