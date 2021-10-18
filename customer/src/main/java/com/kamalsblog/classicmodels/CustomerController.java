f959d198b9fdea8afe310854d285e06627cae494
kamal <kamal@tef>
2021-10-18 13:12:03 +0530

    cloud run commit

--- customer/src/main/java/com/kamalsblog/classicmodels/CustomerController.java
+++ customer/src/main/java/com/kamalsblog/classicmodels/CustomerController.java
@@ -12,6 +12,7 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +34,11 @@ public class CustomerController {
 	@Autowired
 	public CustomerController(MongoTemplate template, CustomerRepository repository) {
 		this.repository = repository;
+	}
+	
+	@RequestMapping(value = "/**",method = RequestMethod.OPTIONS)     
+	public ResponseEntity handle() {         
+		return new ResponseEntity(HttpStatus.OK);     
 	}
 
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
